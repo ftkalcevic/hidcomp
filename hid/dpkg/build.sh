8bbe74f6a33f6562841d404f97d9c407f9fc82cb
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2009-07-30 09:02:03 +0000

    update packaging

--- hid/dpkg/build.sh
+++ hid/dpkg/build.sh
@@ -5,18 +5,30 @@ mkdir debian/usr
 mkdir debian/usr/bin
 mkdir debian/usr/share
 mkdir debian/usr/share/doc
+mkdir debian/usr/share/doc/hidcomp
 mkdir debian/usr/share/man
 mkdir debian/usr/share/man/man1
 mkdir debian/usr/share/man/man9
+#ln -s DEBIAN/control debian/control
 
 cp ../hidcomp/release/hidcomp debian/usr/bin
 cp ../hidconfig/release/hidconfig debian/usr/bin
+#dpkg-shlibdeps --ignore-missing-info  debian/usr/bin/hidconfig debian/usr/bin/hidcomp
+
+strip debian/usr/bin/hidcomp
+strip debian/usr/bin/hidconfig
+
 cp src/hidcomp.9 debian/usr/share/man/man9/hidcomp.9
 gzip -9 -f debian/usr/share/man/man9/hidcomp.9
+
 cp src/hidconfig.1 debian/usr/share/man/man1/hidconfig.1
 gzip -9 -f debian/usr/share/man/man1/hidconfig.1
+
 cp src/copyright debian/usr/share/doc/hidcomp/copyright
+cp src/changelog debian/usr/share/doc/hidcomp/changelog
+gzip -9 debian/usr/share/doc/hidcomp/changelog
+
 cp src/control debian/DEBIAN/control
 
-dpkg-deb --build debian .
+fakeroot dpkg-deb --build debian .
 
